18135b422aad04ee99bd0dc1e660e9bbe6957e5f
dn100189bva <vasilij.burlaka@privatbank.ua>
2015-05-28 20:11:37 +0300

    upd

--- lib/app.rb
+++ lib/app.rb
@@ -2,42 +2,59 @@ require 'codebreaker'
 
 class App 
   
-  DATA_PATH = 'data/'
-	attr_reader :game
+  DATA_FOLDER = 'data'
+  attr_reader :game
+  attr_reader :history
 
-  def initialize(game_file)
- #   @game_file = DATA_PATH + game_file
-    @game = game_init(DATA_PATH + game_file)
-	end
+  def initialize(file_name)
+    game_file = DATA_FOLDER + '/' + file_name
+    hist_file = game_file + '.hist'
+    game_init(game_file, hist_file)
 
-  def game_init(file_name)
-    puts file_name
-    if File.exist?(file_name)
-      game = load(file_name)
+  end
+
+  def game_init(game_file, hist_file)
+    if File.exist?(game_file)
+      @game = load(game_file)
+
+      if File.exist?(hist_file)
+        @history = load(hist_file)
+      else
+        hist_init(hist_file)
+      end
     else
-      game = Codebreaker::Game.new
-      game.start
-      save(file_name)
+      @game = Codebreaker::Game.new
+      @game.start
+      save(@game, game_file)
+
+      hist_init(hist_file)
     end
-    game
+  end
+
+  def hist_init(hist_file)
+    @history = {}
+    save(@history, hist_file)
   end
 
   def load(file_name)
     Marshal.load(File.read(file_name))
   end
 
-  def save(file_name)
+  def save(obj, file_name)
     File.open(file_name, 'w') {|f| 
-      f.write(Marshal.dump(@game)) 
+      f.write(Marshal.dump(obj))
     }
   end
 end
 
+#=begin
 aplication = App.new('test.marshal1')
 puts aplication.game.round_number
-puts aplication.game.check(4511).to_s
-puts aplication.game.check(4511).to_s
-puts aplication.game.check(4511).to_s
+puts aplication.game.check(1234).to_s
+puts aplication.game.check(5612).to_s
+puts aplication.game.check(3456).to_s
+puts aplication.game.check(3663).to_s
+
 puts aplication.game.score
 puts aplication.game.round_number
-aplication.save('data/test.marshal1')
+#=end
--- lib/racker.rb
+++ lib/racker.rb
@@ -12,20 +12,19 @@ class Racker
   def response
     case @request.path
     when "/" then Rack::Response.new(render("index.html.erb"))
-    when "/requests" then 
-      Rack::Response.new(requests("requests.rb"))
-    else Rack::Response.new("Not Found", 404)
+    when "/check" then
+      Rack::Response.new do |request|
+        req = request.inspect
+        puts request.redirect('/')
+      end
+    else
+        Rack::Response.new("Not Found", 404)
     end
   end
    
   def render(template)
     path = File.expand_path("../views/#{template}", __FILE__)
     ERB.new(File.read(path)).result(binding)
-  end
-
-  def requests(template)
-    path = File.expand_path("../#{template}", __FILE__)
-	require path  
   end
 
 end
